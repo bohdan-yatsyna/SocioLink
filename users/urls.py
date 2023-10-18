27b31ed9344f6f3895be2bb187eab37b60935482
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-18 19:51:29 +0300

    Add additional urls to user app for login & token obtain/refresh

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView
+)
 
 from users.views import SignupUserView, ManageUserView
 
@@ -7,5 +10,7 @@ app_name = "users"
 
 urlpatterns = [
     path("signup/", SignupUserView.as_view(), name="signup"),
-    path("me/", ManageUserView.as_view(), name="manage"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair_and_login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage_user_details"),
 ]
