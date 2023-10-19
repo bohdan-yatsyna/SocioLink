e1c4e3882489987f3ce1b610008534e356459290
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-19 12:34:03 +0300

    Add user_activity/ endpoint to uers urls, update login/endoint with corresponding view

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView, TokenRefreshView
-)
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from users.views import SignupUserView, ManageUserView
+from users.views import (
+    CustomTokenObtainPairView,
+    ManageUserView,
+    UserLastActivityView,
+    SignupUserView,
+)
 
 
 app_name = "users"
 
 urlpatterns = [
     path("signup/", SignupUserView.as_view(), name="signup"),
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair_and_login"),
+    path("login/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair_and_login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", ManageUserView.as_view(), name="manage_user_details"),
+    path("user_activity/", UserLastActivityView.as_view(), name="user_activity"),
 ]
