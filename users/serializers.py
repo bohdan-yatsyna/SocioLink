80c58d4d890af9b5abe9ebbd31c2886b072be922
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-19 13:09:03 +0300

    Add type annotations to user/serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,11 @@
+from typing import Dict, Any
+
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -18,12 +22,12 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
 
-    def create(self, validated_data):
+    def create(self, validated_data: Dict[str, Any]):
         """Create a new user with encrypted password and return it"""
 
         return get_user_model().objects.create_user(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: User, validated_data: Dict[str, Any]) -> User:
         """Update a user, set the password correctly and return it"""
 
         password = validated_data.pop("password", None)
@@ -37,7 +41,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    def validate(self, attrs):
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
         data = super().validate(attrs)
 
         self.user.last_login_at = timezone.now()
