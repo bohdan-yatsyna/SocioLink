b29762285a9bb29e6e95b52d84d768a973dce50f
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-21 17:53:01 +0300

    Create serializers.py with PostSerializer for posts app

---
+++ posts/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from posts.models import Post
+
+from rest_framework import serializers
+from posts.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+
+    class Meta:
+        model = Post
+        fields = ("id", "author", "title", "text", "created_at")
