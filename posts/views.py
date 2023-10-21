a1fe34b5809a6854600a2f9e14efe46e3080fead
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-21 18:01:40 +0300

    Create PostCreateListView & PostRetrieveUpdateDestroyView in posts/views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from posts.models import Post
+from posts.serializers import PostSerializer
+from posts.permissions import IsAuthorOrReadOnly
 
-# Create your views here.
+
+class PostCreateListView(generics.ListCreateAPIView):
+    queryset = Post.objects.select_related("author")
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
