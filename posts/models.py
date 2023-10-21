24b5d7d81301cf5654a31c9ba4caa553358e3933
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-21 17:51:32 +0300

    Create Post and Like models for posts app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    title = models.CharField(max_length=255, blank=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self) -> str:
+        return f"{self.title}, {self.created_at}"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        to=Post,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    liked_by = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="likes",
+    )
+    liked_at = models.DateTimeField(auto_now_add=True)
