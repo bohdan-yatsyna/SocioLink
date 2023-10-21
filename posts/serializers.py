176f1b35b6debdbbf99c9a54d2576205c597ee34
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-21 17:54:03 +0300

    Delete redundant imports from posts/serializers.py

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,6 @@
 from rest_framework import serializers
 from posts.models import Post
 
-from rest_framework import serializers
-from posts.models import Post
-
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
