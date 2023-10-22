00987d6b1c1b0b94d0c3192d96dc84d9eb1cbd0c
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-22 23:11:51 +0300

    Change ALLOWED_HOSTS in settings.py

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["app", "127.0.0.1"]
 
 
 # Application definition
