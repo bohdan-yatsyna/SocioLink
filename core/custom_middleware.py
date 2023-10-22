772ef1226d87af421d68a672fd0f7ee4243d98fe
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-22 21:06:55 +0300

    Add type annotations to all methods in custom_middleware.py

--- core/custom_middleware.py
+++ core/custom_middleware.py
@@ -1,11 +1,14 @@
+from typing import Callable
+
+from django.http import HttpRequest, HttpResponse
 from django.utils import timezone
 
 
 class LastUserRequestMiddleware:
-    def __init__(self, get_response):
+    def __init__(self, get_response: Callable) -> Callable:
         self.get_response = get_response
 
-    def __call__(self, request):
+    def __call__(self, request: HttpRequest) -> HttpResponse:
         response = self.get_response(request)
 
         if request.user.is_authenticated:
