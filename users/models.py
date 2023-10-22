98a5ef72d24bdb0eca34837e98d618a3842b6ce6
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-22 21:02:08 +0300

    Add annotations to all methods in users/models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Any
+
 from django.contrib.auth.models import (
     AbstractUser,
     BaseUserManager,
@@ -13,7 +17,7 @@ class UserManager(BaseUserManager):
 
     use_in_migrations = True
 
-    def _create_user(self, email: str, password: str, **extra_fields):
+    def _create_user(self, email: str, password: str, **extra_fields: Any) -> User:
         """Create and save a User with the given email and password."""
         if not email:
             raise ValueError("The given email must be set")
@@ -25,14 +29,14 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_user(self, email: str, password: str = None, **extra_fields):
+    def create_user(self, email: str, password: str = None, **extra_fields: Any) -> User:
         """Create and save a regular User with the given email and password."""
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
 
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email: str, password: str, **extra_fields):
+    def create_superuser(self, email: str, password: str, **extra_fields: Any) -> User:
         """Create and save a SuperUser with the given email and password."""
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
