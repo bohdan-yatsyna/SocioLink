c5e2a2eeb2218ca3d5a7c2d3eb9707a3ba58040b
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-22 12:53:14 +0300

    Add LikeAnalyticsView, update PostUnlikeView, update permission name for PostRetrieveUpdateDestroyView

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+
+from django.db.models import Count, F
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, serializers, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from posts.models import Post, Like
 from posts.serializers import PostSerializer, LikeSerializer
-from posts.permissions import IsAuthorOrReadOnly
+from posts.permissions import IsPostAuthorOrReadOnly
 
 
 class PostCreateListView(generics.ListCreateAPIView):
@@ -18,7 +23,7 @@ class PostCreateListView(generics.ListCreateAPIView):
 class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.select_related("author")
     serializer_class = PostSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (IsPostAuthorOrReadOnly,)
 
 
 class PostLikeView(generics.CreateAPIView):
@@ -37,7 +42,7 @@ class PostLikeView(generics.CreateAPIView):
 
 
 class PostUnlikeView(generics.DestroyAPIView):
-    queryset = Like.objects.all()
+    queryset = Like.objects.select_related("liked_by")
     lookup_field = "post__id"
     lookup_url_kwarg = "post_id"
 
@@ -51,10 +56,61 @@ class PostUnlikeView(generics.DestroyAPIView):
 
         return instance
 
-    def delete(self, request, *args, **kwargs):
-        instance = self.get_object()
+    def perform_destroy(self, instance):
         instance.delete()
-        return Response(
-            {"detail": "Post is unliked."},
-            status=status.HTTP_200_OK
-        )
+
+
+class LikeAnalyticsView(APIView):
+    """View for providing like analytics annotated by days"""
+
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        """
+            Retrieve the number of likes aggregated by day according to
+            provided date range. Requires 'date_from' and 'date_to' as
+            query parameters in the format YYYY-MM-DD.
+        """
+        date_from = self.request.query_params.get("date_from")
+        date_to = self.request.query_params.get("date_to")
+
+        if not date_from or not date_to:
+            return Response(
+                {"error": "Both 'date_from' and 'date_to' are required."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            date_from = datetime.strptime(date_from, "%Y-%m-%d").date()
+            date_to = datetime.strptime(date_to, "%Y-%m-%d").date()
+        except ValueError:
+            return Response(
+                {
+                    "error": "Date format is invalid. "
+                             "It supposed to be YYYY-MM-DD."
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if date_to < date_from:
+            return Response(
+                {
+                    "error": "The 'date_to' must to be greater than "
+                             "'date_from' or equal."
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        aggregated_likes = Like.objects.filter(
+            liked_date__range=[date_from, date_to]
+        ).annotate(date=F("liked_date")).values("date").annotate(
+            likes_count=Count("id")
+        ).order_by("date")
+
+        if not aggregated_likes:
+            return Response(
+                {"message": "There are no likes in the provided time range."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        return Response(list(aggregated_likes))
