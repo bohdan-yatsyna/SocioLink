8c42ec8d37d98bb3f31e77df389500e253f25fa9
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-22 12:50:54 +0300

    Update custom IsPostAuthorOrReadOnly in posts/permissions.py

--- posts/permissions.py
+++ posts/permissions.py
@@ -1,14 +1,16 @@
 from rest_framework import permissions
 
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
+class IsPostAuthorOrReadOnly(permissions.BasePermission):
     """
     Custom permission to allow only authors of a post to edit or delete it.
     """
 
     def has_object_permission(self, request, view, obj):
-
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.author == request.user
+        if hasattr(obj, "author"):
+            return obj.author == request.user
+
+        return False
