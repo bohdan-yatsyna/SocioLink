2a2e49d928d7d6c3c2a9177c5be4f5425f45a8f3
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-22 12:55:48 +0300

    Update UserLastActivityView in a way that only user himself or admin user will have access to user_activity endpoint

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, exceptions
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from users.models import User
 from users.serializers import (
     CustomTokenObtainPairSerializer,
     UserLastActivitySerializer,
@@ -31,4 +33,12 @@ class UserLastActivityView(generics.RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
-        return self.request.user
+        user_id = self.kwargs["user_id"]
+        user = get_object_or_404(User, id=user_id)
+
+        if self.request.user == user or self.request.user.is_superuser:
+            return user
+
+        raise exceptions.PermissionDenied(
+            detail="Access denied, you can not see this user's activity."
+        )
